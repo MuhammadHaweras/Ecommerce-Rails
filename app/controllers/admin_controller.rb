2ebbe45f09e9d5933e81560476ae90aaf4ea1f59
MuhammadHaweras <haweras.noor@gmail.com>
2024-02-27 08:59:49 -0800

    added orders crud, OrderProduct table and devise logout

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,6 +3,11 @@ class AdminController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-
+    @orders = Orders.where(fulfilled: false).order(created_at: :desc).take(5)
+    @quick_stats = {
+      sales: Order.where(created_at: Time.now.midnight .. Time.now).count,
+      avg_sales: Order.where(created_at: Time.now.midnight .. Time.now).average(:total).round(),
+      per_sale: OrderProduct.joins(:order).where(orders: {created_at: Time.now.midnight .. Time.now }).average(:quantity)
+    }
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   belongs_to :category
 
   has_many :stocks
+  has_many :order_products
 
   has_many_attached :images
+
 end
